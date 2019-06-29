9013d57d331f95ee4a91ee4f3b6409384f5b3851
David Schramm <david.schramm@live.com>
2019-06-29 13:17:25 +0200

    remove of temporary file

--- keyman_client.sh
+++ keyman_client.sh
@@ -11,4 +11,6 @@ tf="$(tempfile)"
 #private key
 < "$tf" sed -En -e '/###/ q;p' > "$2"
 #public key
-< "$tf" sed -En -e '/###/ bpri;bend' -e ':pri n;p;bpri' -e ':end' > "$3" 
+< "$tf" sed -En -e '/###/ bpri;bend' -e ':pri n;p;bpri' -e ':end' > "$3"
+
+rm "$tf"
